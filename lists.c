ee4b73aef6dc94385c19bc28a7f9bf1419cfa172
NaokiNakao <naokinakao.nn@gmail.com>
2021-01-22 01:23:48 -0500

    Configuracion inicial

--- lists.c
+++ lists.c
@@ -1,1 +1,10 @@
+/*
+   Librería para la implementación de listas.
+   (Archivo principal)
 
+   Autor : Naoki Nakao
+*/
+
+#include "lists.h"
+
+/* Prototipos de función */
--- lists.h
+++ lists.h
@@ -1,1 +1,20 @@
+/*
+   Librería para la implementación de listas.
+   (Archivo de cabecera)
 
+   Autor : Naoki Nakao
+*/
+
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+   int value; // almacena el valor del nodo
+   struct Node* next; // apunta al nodo siguiente
+   struct Node* prev; // apunta al nodo anterior
+}List;
+
+#endif // _LISTS_H_
